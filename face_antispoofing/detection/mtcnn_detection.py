759c9c9b81baa2e16c6ea5483d24507617855189
AlexWolfov <git_user@mail.ru>
2023-05-19 09:23:11 +0300

    Final version

--- face_antispoofing/detection/mtcnn_detection.py
+++ face_antispoofing/detection/mtcnn_detection.py
@@ -3,7 +3,7 @@ from facenet_pytorch import MTCNN
 
 class MTCNNDetector:
     def __init__(self):
-        self.mtcnn = MTCNN(min_face_size=200)  # keep_all=True, )
+        self.mtcnn = MTCNN(select_largest=True, min_face_size=200)
 
     def detect_face(self, frame):
         faces, _ = self.mtcnn.detect(frame)
--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import cv2
 import numpy as np
 from face_antispoofing.antispoofing import antispoof
 from face_antispoofing.detection import face_detector, mtcnn_detection
-from models import mobilenet_antispoof
+from models import mobilenet_antispoof, resnet_antispoof
 
 vid = cv2.VideoCapture(0)
 model = antispoof.Antispoof(mobilenet_antispoof.MobileNetAntispoof())
 detector = face_detector.FaceDetector(mtcnn_detection.MTCNNDetector())
-PADDING = 0
+PADDING = 10
 
-FRAMES_PER_CALCULATION = 1  # How often the result is recalculated
+FRAMES_PER_CALCULATION = 5  # How often the result is recalculated
 
 frame_counter = 0
 logits_sum = np.array([[0, 0]], dtype=float)
-prediction_sum = 0
+prediction_sum = -1
 
 while True:
     ret, frame = vid.read()
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
     try:
         faces = detector.detect_face(frame)
@@ -31,9 +34,14 @@ while True:
 
             prediction, logits = model.model_predict(frame_rgb[x0:x1][y0:y1])
 
+            # if cv2.waitKey(1) & 0xFF == ord(" "):
+            #    prediction_sum = -1
+            #    frame_counter = 1
+
             if FRAMES_PER_CALCULATION > 1:
                 frame_counter += 1
                 logits_sum += logits
+                logits = logits_sum / frame_counter
 
                 if FRAMES_PER_CALCULATION == frame_counter:
                     prediction_sum = np.argmax(logits_sum, axis=1)
@@ -41,37 +49,35 @@ while True:
                     frame_counter = 1
 
                 prediction = prediction_sum
-                logits = logits_sum / frame_counter
+
+            print(prediction)
 
             if prediction:
-                cv2.rectangle(frame, (x0, y0), (x1, y1), (0, 255, 0), 2)
+                if prediction != -1:
+                    color = (0, 255, 0)
+                    prediction = "REAL"
+                else:
+                    color = (0, 255, 255)
+                    prediction = "LOADING..."
+            else:
+                color = (0, 0, 255)
+                prediction = "SPOOF"
 
-                (w, h), _ = cv2.getTextSize("REAL", cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)
-                cv2.rectangle(frame, (x0, y0 - h), (x0 + w, y0), (0, 255, 0), -1)
+            cv2.rectangle(frame, (x0, y0), (x1, y1), color, 2)
 
-                cv2.putText(frame, "REAL", (x0, y0), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
-                (w, h), _ = cv2.getTextSize("real", cv2.FONT_HERSHEY_SIMPLEX, 0.55, 1)
-                cv2.putText(frame, f"real {logits[0][1]}", (x0, y1 + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-                cv2.putText(
-                    frame, f"spoof {logits[0][0]}", (x0, y1 + 2 * h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1
-                )
-                continue
-            cv2.rectangle(frame, (x0, y0), (x1, y1), (0, 0, 255), 2)
-
-            (w, h), _ = cv2.getTextSize("SPOOF", cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)
-            cv2.rectangle(frame, (x0, y0 - h), (x0 + w, y0), (0, 0, 255), -1)
-            cv2.putText(frame, "SPOOF", (x0, y0), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
+            (w, h), _ = cv2.getTextSize(prediction, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)
+            cv2.rectangle(frame, (x0, y0 - h), (x0 + w, y0), color, -1)
+            cv2.putText(frame, prediction, (x0, y0), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
 
             (w, h), _ = cv2.getTextSize("real", cv2.FONT_HERSHEY_SIMPLEX, 0.55, 1)
-            cv2.putText(frame, f"real {logits[0][1]}", (x0, y1 + 2 * h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            cv2.putText(frame, f"spoof {logits[0][0]}", (x0, y1 + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            cv2.putText(frame, f"real {logits[0][1]}", (x0, y1 + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            cv2.putText(frame, f"spoof {logits[0][0]}", (x0, y1 + 2 * h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
 
     except:
         pass
 
     cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+
 
 vid.release()
 cv2.destroyAllWindows()
